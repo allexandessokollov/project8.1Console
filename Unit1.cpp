2d5469182991380ae269039b4d24a0962f443499
allexandessokollov <64951280+allexandessokollov@users.noreply.github.com>
2020-12-27 20:24:46 +0300

    3343
    3343

--- Unit1.cpp
+++ Unit1.cpp
@@ -12,13 +12,16 @@
 void addElement(char fileName[]);
 float avgGrade(int arr[]);
 void showDataFromFile(char fileName[]);
+void createFile(char fileName[]);
+void printExellentStudents(char fileName[]);
 
 
 const int ARR_SIZE = 4;
+const int MAX_NAME_SIZE = 128;
 
 struct student
 {
-    char name[64];
+    char name[MAX_NAME_SIZE];
     int yearOfBirdth;
     int group;
     int grades[ARR_SIZE]; // 0 - physics, 1 - math, 2 - Comp Sience, 3 - chemistry
@@ -29,21 +32,48 @@ int main()
 {
 
 
-    char fileName[128];
+    char fileName[MAX_NAME_SIZE];
     int userChoice;
-    printf("enter file name\n");
-    scanf("%s", &fileName);
+    bool toSwitch = true;
 
 
+    while(toSwitch)
+    {
+        printf("Enter 1 to create file\nEnter 2 to show data\n");
+        printf("Enter 3 to add element\n");
+        printf("Enter 4 to see exellent students\nEnter 5 to close app\n");
+        scanf("%d", &userChoice);
 
+        switch(userChoice)
+        {
+            case 1:
+            {
+                createFile(fileName);
+                break;
+            }
+            case 2:
+            {
+                showDataFromFile(fileName);
+                break;
+            }
+            case 3:
+            {
+                addElement(fileName);
+                break;
+            }
+            case 4:
+            {
+                printExellentStudents(fileName);
+                break;
+            }
+            case 5:
+            {
+                toSwitch = false;
+                break;
+            }
+        }
+    }
 
-   
-    showDataFromFile(fileName);
-
-
-    puts("\n\n\nPress any key ... ");
-    getch();
-    getch();
 
     return 0;
 }
@@ -124,5 +154,57 @@ void showDataFromFile(char fileName[])
         }
     }
     fclose (file);
+}
+
+
+void createFile(char fileName[])
+{
+    char temp[128];
+    printf("enter file name\n");
+    scanf("%s", &temp);
+
+    for(int i = 0; i < MAX_NAME_SIZE; i++)
+    {
+        fileName[i] = temp[i];
+    }
+
+    FILE *file;
+
+    if ((file = fopen(fileName, "w")) == NULL)
+    {
+        printf("\nCannot create file.\n");
+    }
+    else
+    {
+        printf("\nfile Created!\n\n");
+    }
+
+}
+
+
+void printExellentStudents(char fileName[])
+{
+    FILE *file;
+    student st;
+
+
+    if ((file = fopen(fileName, "r")) == NULL)
+    {
+        printf("\nCannot open file.\n");
+    }
+    else
+    {
+        while((fscanf(file, "%s %d %d %d %d %d %d %lf\n", &st.name, &st.yearOfBirdth, &st.group,
+             &st.grades[0], &st.grades[1], &st.grades[2], &st.grades[3], &st.avgGrade))!= EOF)
+        {
+            if(st.avgGrade == 8 || st.avgGrade == 9 || st.avgGrade == 10)
+            {
+            printf("\n%s %d %d %d %d %d %d %lf\n", st.name, st.yearOfBirdth, st.group,
+             st.grades[0], st.grades[1], st.grades[2], st.grades[3], st.avgGrade);
+            }
+        }
+    }
+    fclose (file);
+
 }
 
